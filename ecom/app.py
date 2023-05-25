8f8b16d8de769df4da0ee34f897e947d8c2fc666
Harshit <inbox.Harshityadav@gmail.com>
2023-05-25 20:51:29 +0530

    PyMongo Added

--- ecom/app.py
+++ ecom/app.py
@@ -4,35 +4,21 @@ from pymongo import MongoClient
 
 
 client = MongoClient()
-client = MongoClient('mongodb+srv://harshityadav:JxsV3y4V7mWl8g1I@cluster0.s9trpdc.mongodb.net/')
-db = client['Ecommerce']
-products_collection = db['products']
-orders_collection = db['order']
-cart_collection = db['cart']
+client = MongoClient(
+    "mongodb+srv://harshityadav:JxsV3y4V7mWl8g1I@cluster0.s9trpdc.mongodb.net/"
+)
+db = client["Ecommerce"]
+products_collection = db["products"]
+orders_collection = db["order"]
+cart_collection = db["cart"]
 
 
 app = Flask(__name__)
 
-# products = [
-#     {"pId": "1", "ProductName": "Noodles", "Price": "55", "Img_Url": "Noodles.png"},
-#     {"pId": "2", "ProductName": "Burger", "Price": "60", "Img_Url": "Burger.png"},
-#     {"pId": "3", "ProductName": "Pizza", "Price": "120", "Img_Url": "Pizza.png"},
-#     {
-#         "pId": "4",
-#         "ProductName": "Cold Coffee",
-#         "Price": "65",
-#         "Img_Url": "Cold Coffee.png",
-#     },
-#     {"pId": "5", "ProductName": "Fries", "Price": "40", "Img_Url": "Fries.png"},
-# ]
-
-# cart = []
-# orders = []
-
 
 @app.route("/")
 def index():
-    return jsonify(message="Index")
+    return jsonify(message="Welcome to the e-commerce application!")
 
 
 # Products
@@ -40,13 +26,8 @@ def index():
 
 @app.route("/products", methods=["GET"])
 def get_products():
-    result = list(products_collection.find({}))
-    print(type(result))
-    print(result)
-    result = json.dumps(result)
-    return result
-    # return products_collection.find({},{'_id':0})
-    # return jsonify(prodcuts=products_collection.find({},{'_id':0}))
+    products = list(products_collection.find({}, {"_id": 0}))
+    return jsonify(products=products)
 
 
 @app.route("/products", methods=["POST"])
@@ -59,69 +40,56 @@ def add_product():
 # Cart
 @app.route("/cart", methods=["GET"])
 def get_cart():
+    cart = list(cart_collection.find({}, {"_id": 0}))
     if len(cart) == 0:
         return jsonify(message="Empty Cart")
     else:
         total = 0
         for item in cart:
             total += int(item["Price"])
-    return jsonify(Cart_Item=cart,Cart_Total_Value=total)
+    return jsonify(Cart_Items=cart, Cart_Total_Value=total)
 
 
 @app.route("/cart", methods=["POST"])
 def add_cart():
-    items=(request.get_json())
-    totalItems = len(items)
-    addedItems = 0
-    if totalItems == 0:
-        return jsonify(message="No Items to add in Cart")
+    item = request.get_json()
+    if products_collection.find_one(item):
+        cart_collection.insert_one(item)
+        return jsonify(
+            message=f"Product '{item['ProductName']}' added to Cart successfully!"
+        )
     else:
-        for item in items:
-            pId = int(item["pId"])
-            for product in products:
-                if int(product["pId"]) == pId:
-                    cart.append(product)
-                    addedItems += 1
-        if addedItems == 0:
-            return jsonify(message="Order Failed: None of the Items are Available to Order")
-        elif (addedItems!=totalItems):
-            return jsonify(message=f"'{totalItems-addedItems}'Product Unavailable to Order")
-        else:
-            return jsonify(message=f"Order of items - '{items}' placed successfully!")
+        return jsonify(message=f"Product '{item['ProductName']}' is unavailable!")
 
 
 @app.route("/clearcart", methods=["GET"])
 def clear_cart():
-    cart.clear()
+    cart_collection.delete_many({})
     return jsonify(message="All items have been Removed from the Cart")
 
 
 # Order
 @app.route("/orders", methods=["GET"])
 def get_orders():
-    return jsonify(orders=orders)
+    orders = list(orders_collection.find({}, {"_id": 0}))
+    if orders:
+        return jsonify(Orders=orders)
+    else:
+        return jsonify(message="You Have'nt placed any orders yet")
 
 
 @app.route("/orders", methods=["POST"])
 def add_order():
-    orderList = request.get_json()
-    addedItems = 0
-    totalItems = len(orderList)
-    if len(orderList) == 0:
-        return jsonify(message="Add an Item to Place an Order")
-    if len(cart) == 0:
-        return jsonify(message="Cart is Empty,add items in cart to place an order")
-    for product in orderList:
-        pId = int(product["pId"])
-        for item in cart:
-            if int(item["pId"]) == pId:
-                orders.append(orderList)
-                addedItems+=1              
-    if addedItems == 0:
-            return jsonify(message="Order Failed: None of the Items are in Cart")
-    elif (addedItems!=totalItems):
-            return jsonify(message=f"'{totalItems-addedItems}'Product not Found in Cart,Please add it to cart first")
+    orderList = list(cart_collection.find({}))
+    if orderList:
+        for item in orderList:
+            orders_collection.insert_one(item)
+        cart_collection.delete_many({})
+        return jsonify(
+            message=f"Order Successful for the following Items : '{orderList}'"
+        )
     else:
-        return jsonify(message=f"Order of items - '{orderList}' placed successfully!")
+        return jsonify(message="No Items are available in the cart")
+
 
 app.run()
